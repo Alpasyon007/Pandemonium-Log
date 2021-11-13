eb7128564117a7ea0d3a1be4fcd548b6c96ac978
Alp <alpasyon07@gmail.com>
2021-11-13 01:21:16 +0000

    Add DEBUG_BREAKs

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include "Logger/Logger.h"
 #include "Asserter/Asserter.h"
+#include "Breaker/Breaker.h"
 
 int main() {
 	int a = 5;
 	char* b = "BYE";
 
 	ASSERT(a < 1);
+
+	DEBUG_BREAK();
 
 	LOG_DEBUG("HELLO %d %s", a, b);
 	LOG_INFO("HELLO %d %s", a, b);
