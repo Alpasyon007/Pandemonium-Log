cae22411b7de514ec57d49522f9cdecd82ad4de9
Alpasyon007 <alpasyon07@gmail.com>
2022-11-05 04:22:27 +0000

    Fix file path formatter

--- headers/Common.h
+++ headers/Common.h
@@ -7,7 +7,14 @@ namespace Common {
 	static char* FormatFileName(char* file) {
 		int count = 0;
 		for(int i = 0; i < strlen(file); i++) {
-			if(file[i] == '/') { count = i + 1; }
+#if(PANDEMONIUM_PLATFORM_WINDOWS) // File path seperators are \ instead of / on windows
+			if(file[i] == '\\')
+#else
+			if(file[i] == '/')
+#endif /* PANDEMONIUM_PLATFORM_WINDOWS */
+			{
+				count = i + 1;
+			}
 		}
 		return file += count;
 	}
@@ -22,7 +29,7 @@ namespace Common {
 		localtime_s(&timeInfo, &now);
 
 		// Time and File
-		sprintf_s(buffer, "[%d %d %d][%d:%d:%d.%d]", timeInfo.tm_mday, timeInfo.tm_mon + 1, timeInfo.tm_year + 1900, timeInfo.tm_hour, timeInfo.tm_min,
+		sprintf_s(buffer, "[%d/%d/%d][%d:%d:%d.%d]", timeInfo.tm_mday, timeInfo.tm_mon + 1, timeInfo.tm_year + 1900, timeInfo.tm_hour, timeInfo.tm_min,
 				  timeInfo.tm_sec, static_cast<int>(std::chrono::duration_cast<std::chrono::milliseconds>(currentTime.time_since_epoch()).count() % 1000));
 
 		return buffer;
